5d03a65969677a96aee940af5fa99d537e9967b8
Nikitayadav12 <ynikita987@gmail.com>
2025-11-17 16:44:33 +0530

    Add files via upload

---
+++ Car-App/Frontend/Src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cars from "./pages/Cars";
+import Services from "./pages/Services";
+import CarDetail from "./components/Cars/CarDetail";
+import OurCars  from "./components/Cars/OurCars";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/carDetail" element={<CarDetail />} />
+          <Route path="/ourCars" element={<OurCars />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
